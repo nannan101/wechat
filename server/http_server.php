e3f178b8200d26708a6bc5c35988943cd5cb0522
qiao nan <1591941875@qq.com>
2019-11-27 11:45:52 +0800

    修改

--- server/http_server.php
+++ server/http_server.php
@@ -15,55 +15,26 @@
  * swoole_http_server
  */
 
-class HttpServer
-{
-    const HOST = '127.0.0.1';
-    const PORT = 9501;
+$server = new Swoole\Http\Server('127.0.0.1', 9501);
+  
+$server->set([
+    'worker_num' => 1
+]);
+$server->on('Start', function(){
+     swoole_set_process_name('simiple_route_master');
+});
+$server->on('ManagerStart', function (){
+    swoole_set_process_name('simiple_route_manager');
+    spl_autoload_register(function ($class){
+        $baseClasspath = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+        $class = __DIR__ . '/' . $baseClasspath;
+        if (is_file($baseClasspath)){
+            require $baseClasspath;
+        }
+    });
+});
 
-    private $_serv = null;
-    
-    public function __construct($host = null, $port = null) {
-        $host = $host ? $host : self::HOST;
-        $port = $port ? $port : self::PORT;
-        $this->_serv = new Swoole\Http\Server($host, $port);
-        $this->_serv->set([
-            'worker_num' => 1
-        ]);
-        $this->_serv->on('Start', [$this,'onStart']);
-        $this->_serv->on('ManagerStart', [$this,'onManagerStart']);
-        $this->_serv->on('WorkerStart', [$this,'onWorkerStart']);
-        $this->_serv->on('Request', [$this,'onRequest']);
-        $this->_serv->start();
-        
-    }
-    
-    public function onStart()
-    {
-        swoole_set_process_name('simiple_route_master');
-    }
-    
-    /**
-     * 给进程mannger进程取别名 
-     */
-    public function onManagerStart()
-    {
-        swoole_set_process_name('simiple_route_manager');
-    }
-    
-    public function onWorkerStart()
-    {
-        swoole_set_process_name('simiple_route_worker');
-        spl_autoload_register(function ($class){
-            $baseClasspath = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-            $class = __DIR__ . '/' . $baseClasspath;
-            if (is_file($baseClasspath)){
-                require $baseClasspath;
-            }
-        });
-    }
-    
-    public function onRequest(\Swoole\Http\Request $request, Swoole\Http\Response $response)
-    {
+$server->on('Request', function ($request,$response ){
         $path_info = explode('/', $request->server['path_info']);
         /*设置控制器*/
         if ( isset($path_info[1]) && !empty($path_info[1])){
@@ -86,6 +57,5 @@ class HttpServer
             }
         }
         $response->end($result);
-    }
-}
-$http_server = new HttpServer();
+});
+$server->start();
